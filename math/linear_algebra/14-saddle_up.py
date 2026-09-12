b8c8c9c209662a397c39f22d7b16794dcc6604fd
ishejaladouce <l.isheja@alustudent.com>
2026-09-12 05:33:25 -0700

    Add function that multiplies two numpy arrays

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Multiplies two numpy arrays."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the product of two numpy arrays."""
+    return np.matmul(mat1, mat2)
